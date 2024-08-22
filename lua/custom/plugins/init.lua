26d733acdd6c03bdd86a4d4b5d7c92c1f1dd344e
Eric Bäckhage <eric.backhage@zenseact.com>
2024-08-22 14:57:32 +0200

    Changed from using Black to using Ruff for Python

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,7 +2,6 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-require 'custom/plugins/none-ls'
 require 'custom/plugins/gruvbox'
 require 'custom/plugins/vim-tmux-navigator'
 
