8fa707ef051f1194619d21557e3f72852fa00d55
tusharvikky <tusharvikky@gmail.com>
2024-11-11 02:34:35 +0530

    [UPDATE] Cleanup keymaps.

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -9,7 +9,6 @@ function Map(mode, lhs, rhs, opts)
   end
   vim.keymap.set(mode, lhs, rhs, options)
 end
-local opts = { noremap = true, silent = true }
 
 --  See `:help hlsearch`
 Map('n', '<Esc>', '<cmd>nohlsearch<CR>', { desc = 'Clear highlights' })
@@ -44,18 +43,22 @@ Map('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 Map('n', '<C-a>', 'gg<S-v>G')
 
 -- Disable continuations
-Map('n', '<Leader>o', 'o<Esc>^Da', { noremap = true, silent = true, desc = 'Insert new line below' }) -- Inserts new line below the current line and sets insert mode
-Map('n', '<Leader>O', 'O<Esc>^Da', { noremap = true, silent = true, desc = 'Insert new line above' }) -- Inserts new line above the current line and sets insert mode
+Map('n', '<Leader>o', 'o<Esc>^Da', { desc = 'Insert new line below' }) -- Inserts new line below the current line and sets insert mode
+Map('n', '<Leader>O', 'O<Esc>^Da', { desc = 'Insert new line above' }) -- Inserts new line above the current line and sets insert mode
 -- nvim-comment
 Map({ 'n', 'v' }, '<leader>/', ':CommentToggle<cr>', { desc = 'Comment Toggle anything' })
 
 -- Move current line/block down/up using Alt+j/k
-Map('n', '<A-j>', ':m .+1<CR>==', { noremap = true, silent = true, desc = 'Move current line/block down' })
-Map('n', '<A-k>', ':m .-2<CR>==', { noremap = true, silent = true, desc = 'Move current line/block up' })
+Map('n', '<A-j>', ':m .+1<CR>==', { desc = 'Move current line/block down' })
+Map('n', '<A-k>', ':m .-2<CR>==', { desc = 'Move current line/block up' })
 
 -- For visual mode (selecting lines)
-Map('v', '<A-j>', ":m '>+1<CR>gv=gv", { noremap = true, silent = true, desc = 'Move current line/block down' })
-Map('v', '<A-k>', ":m '<-2<CR>gv=gv", { noremap = true, silent = true, desc = 'Move current line/block up' })
+Map('v', '<A-j>', ":m '>+1<CR>gv=gv", { desc = 'Move current line/block down' })
+Map('v', '<A-k>', ":m '<-2<CR>gv=gv", { desc = 'Move current line/block up' })
+
+-- Better movement key to shift text left and right
+Map('v', '<', '<gv', { desc = 'Intending lines left' })
+Map('v', '>', '>gv', { desc = 'Intending lines right' })
 
 -- [[ File Explorer ]]
 
@@ -72,8 +75,8 @@ end, { desc = 'Toggle File Explorer' })
 -- window management
 -- New tab
 Map('n', 'te', ':tabedit<Return>')
-Map('n', '<tab>', ':tabnext<Return>', opts)
-Map('n', '<s-tab>', ':tabprev<Return>', opts)
+Map('n', '<tab>', ':tabnext<Return>')
+Map('n', '<s-tab>', ':tabprev<Return>')
 
 -- Resize window
 Map('n', '<C-w><left>', '<C-w><')
