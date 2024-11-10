a49f61fc9e68bf4f2db43d3547d19d382c0138ae
tusharvikky <tusharvikky@gmail.com>
2024-11-11 02:12:17 +0530

    [UPDATE] keymap, Map function and restructure file.

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,19 +1,21 @@
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
+function Map(mode, lhs, rhs, opts)
+  local options = { noremap = true, silent = true }
+  mode = mode or 'n'
+  if opts then
+    options = vim.tbl_extend('force', options, opts)
+  end
+  vim.keymap.set(mode, lhs, rhs, options)
+end
 local opts = { noremap = true, silent = true }
--- local keymap = function(keys, func, opt, mode)
---   opt = opt or {}
---   mode = mode or 'n'
---   vim.keymap.set(mode, keys, func, opt)
--- end
 
--- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>', { desc = 'Clear highlights' })
+Map('n', '<Esc>', '<cmd>nohlsearch<CR>', { desc = 'Clear highlights' })
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+Map('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
@@ -21,61 +23,43 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 --
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+Map('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+-- Map('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+-- Map('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+-- Map('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+-- Map('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-
--- window management
--- New tab
-vim.keymap.set('n', 'te', ':tabedit<Return>')
-vim.keymap.set('n', '<tab>', ':tabnext<Return>', opts)
-vim.keymap.set('n', '<s-tab>', ':tabprev<Return>', opts)
-
--- Resize window
-vim.keymap.set('n', '<C-w><left>', '<C-w><')
-vim.keymap.set('n', '<C-w><right>', '<C-w>>')
-vim.keymap.set('n', '<C-w><up>', '<C-w>+')
-vim.keymap.set('n', '<C-w><down>', '<C-w>-')
-
-vim.keymap.set('n', '<leader>to', '<cmd>tabnew<CR>', { desc = 'Open new tab' }) -- open new tab
-vim.keymap.set('n', '<leader>tx', '<cmd>tabclose<CR>', { desc = 'Close current tab' }) -- close current tab
-vim.keymap.set('n', '<leader>tn', '<cmd>tabn<CR>', { desc = 'Go to next tab' }) --  go to next tab
-vim.keymap.set('n', '<leader>tp', '<cmd>tabp<CR>', { desc = 'Go to previous tab' }) --  go to previous tab
-vim.keymap.set('n', '<leader>tf', '<cmd>tabnew %<CR>', { desc = 'Open current buffer in new tab' }) --  move current buffer to new tab
+Map('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+Map('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+Map('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+Map('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- Select all
-vim.keymap.set('n', '<C-a>', 'gg<S-v>G')
+Map('n', '<C-a>', 'gg<S-v>G')
 
 -- Disable continuations
-vim.keymap.set('n', '<Leader>o', 'o<Esc>^Da', { noremap = true, silent = true, desc = 'Insert new line below' }) -- Inserts new line below the current line and sets insert mode
-vim.keymap.set('n', '<Leader>O', 'O<Esc>^Da', { noremap = true, silent = true, desc = 'Insert new line above' }) -- Inserts new line above the current line and sets insert mode
+Map('n', '<Leader>o', 'o<Esc>^Da', { noremap = true, silent = true, desc = 'Insert new line below' }) -- Inserts new line below the current line and sets insert mode
+Map('n', '<Leader>O', 'O<Esc>^Da', { noremap = true, silent = true, desc = 'Insert new line above' }) -- Inserts new line above the current line and sets insert mode
 -- nvim-comment
-vim.keymap.set({ 'n', 'v' }, '<leader>/', ':CommentToggle<cr>', { desc = 'Comment Toggle anything' })
+Map({ 'n', 'v' }, '<leader>/', ':CommentToggle<cr>', { desc = 'Comment Toggle anything' })
 
 -- Move current line/block down/up using Alt+j/k
-vim.keymap.set('n', '<A-j>', ':m .+1<CR>==', { noremap = true, silent = true, desc = 'Move current line/block down' })
-vim.keymap.set('n', '<A-k>', ':m .-2<CR>==', { noremap = true, silent = true, desc = 'Move current line/block up' })
+Map('n', '<A-j>', ':m .+1<CR>==', { noremap = true, silent = true, desc = 'Move current line/block down' })
+Map('n', '<A-k>', ':m .-2<CR>==', { noremap = true, silent = true, desc = 'Move current line/block up' })
 
 -- For visual mode (selecting lines)
-vim.keymap.set('v', '<A-j>', ":m '>+1<CR>gv=gv", { noremap = true, silent = true, desc = 'Move current line/block down' })
-vim.keymap.set('v', '<A-k>', ":m '<-2<CR>gv=gv", { noremap = true, silent = true, desc = 'Move current line/block up' })
+Map('v', '<A-j>', ":m '>+1<CR>gv=gv", { noremap = true, silent = true, desc = 'Move current line/block down' })
+Map('v', '<A-k>', ":m '<-2<CR>gv=gv", { noremap = true, silent = true, desc = 'Move current line/block up' })
 
 -- [[ File Explorer ]]
 
-vim.keymap.set('n', '<leader>e', function()
+Map('n', '<leader>e', function()
   if vim.api.nvim_buf_get_option(0, 'filetype') == 'netrw' then
     vim.cmd 'bprev' -- Switch back to previous buffer
   else
@@ -84,6 +68,24 @@ vim.keymap.set('n', '<leader>e', function()
 end, { desc = 'Toggle File Explorer' })
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
+
+-- window management
+-- New tab
+Map('n', 'te', ':tabedit<Return>')
+Map('n', '<tab>', ':tabnext<Return>', opts)
+Map('n', '<s-tab>', ':tabprev<Return>', opts)
+
+-- Resize window
+Map('n', '<C-w><left>', '<C-w><')
+Map('n', '<C-w><right>', '<C-w>>')
+Map('n', '<C-w><up>', '<C-w>+')
+Map('n', '<C-w><down>', '<C-w>-')
+
+Map('n', '<leader>to', '<cmd>tabnew<CR>', { desc = 'Open new tab' }) -- open new tab
+Map('n', '<leader>tx', '<cmd>tabclose<CR>', { desc = 'Close current tab' }) -- close current tab
+Map('n', '<leader>tn', '<cmd>tabn<CR>', { desc = 'Go to next tab' }) --  go to next tab
+Map('n', '<leader>tp', '<cmd>tabp<CR>', { desc = 'Go to previous tab' }) --  go to previous tab
+Map('n', '<leader>tf', '<cmd>tabnew %<CR>', { desc = 'Open current buffer in new tab' }) --  move current buffer to new tab
 
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
