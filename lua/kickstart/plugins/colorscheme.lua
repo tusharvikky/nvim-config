c7c510e112f7e439b6378d7e14331a0a349a7070
tusharvikky <tusharvikky@gmail.com>
2025-03-10 00:21:00 +0530

    [UPDATE] colorscheme changed to jellybeans

--- lua/kickstart/plugins/colorscheme.lua
+++ lua/kickstart/plugins/colorscheme.lua
@@ -5,14 +5,18 @@ return {
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     -- 'folke/tokyonight.nvim',
-    'nyoom-engineering/oxocarbon.nvim',
+    'rktjmp/lush.nvim',
+    'metalelf0/jellybeans-nvim',
+    -- 'nyoom-engineering/oxocarbon.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       -- vim.cmd.colorscheme 'tokyonight-night'
-      vim.cmd.colorscheme 'oxocarbon'
+      -- vim.cmd.colorscheme 'oxocarbon'
+      vim.cmd.colorscheme 'jellybeans-nvim'
+      vim.opt.termguicolors = true
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
